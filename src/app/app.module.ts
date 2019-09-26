7e0cecc66e29e54ce2943d33f2e9fb836bba063b
tri_ <rurouniadhi@gmail.com>
2019-09-26 21:50:23 +0700

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IndexComponent } from './index/index.component';
 
+import { AngularFullpageModule } from '@fullpage/angular-fullpage';
+import { MatDialogModule } from '@angular/material/dialog';
+
+import { StepListComponent } from './shared/step-list/step-list.component';
+import { ModalComponent } from './shared/modal/modal.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    StepListComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFullpageModule,
+    MatDialogModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [ModalComponent],
 })
 export class AppModule { }
