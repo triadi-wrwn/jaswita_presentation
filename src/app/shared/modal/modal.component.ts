d40dfd06cc85b7f1eca775941df584c23bc419f9
tri_ <rurouniadhi@gmail.com>
2019-09-27 10:58:13 +0700

    add modal

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface DialogData {
+  title: string;
+  address: string;
+  img1: string;
+  img2: string;
+  inv: string;
+  share: string;
+  irr: string;
+  der: string;
+  npv: string;
+  pi: string;
+  pp: string;
+}
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) {}
 
 }
